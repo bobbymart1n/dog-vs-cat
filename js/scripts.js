ac1fc1bf081c28fc47347c90c13348e30fff31fb
Bobby Martin <robert.martin.619@gmail.com>
2018-03-20 19:10:08 -0700

    Add image manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").prepend("<li>Bork!</li>");
   });
   $("button#bark").click(function() {
-    $("ul#dog").prepend("<li>Bork!</li>");
+    $("ul#dog").prepend("<li>Bark!</li>");
+    $("ul#cat").prepend("<li>Mauw!</li>");
+  });
+  $("button#image-button").click(function() {
+     $("#image-container").after('<img src="img/suh-dude.jpg" id="img" />')
+     $("img#img").click(function() {
+        $(this).remove();
+     });
   });
 });
